0fa72e83b07ddf59dd3bf75ed014cb7bb976cb6a
André Kaoru Fuziyama <andrefuzi@MacBook-Pro-de-Andre.local>
2020-08-01 01:22:40 -0300

    Add data validation on post methods

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import multer from 'multer'
 import multerConfig from './config/multer'
+import { celebrate, Joi } from 'celebrate'
 
 import CollectPointServices from './services/CollectPointServices'
 import ItemServices from './services/ItemServices'
@@ -16,6 +17,21 @@ routes.get('/items', itemServices.getAll)
 routes.get('/collect-points', collectPointServices.getAll)
 routes.get('/collect-points/:id', collectPointServices.getById)
 
-routes.post('/collect-points', upload.single('image'), collectPointServices.create)
+routes.post('/collect-points', upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().max(2).required(),
+      items: Joi.string().required()
+    })
+  }, {
+    abortEarly: false
+  }),
+  collectPointServices.create)
 
 export default routes
